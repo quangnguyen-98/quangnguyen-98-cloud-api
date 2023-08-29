09d74ef0f7ddd0f71e87afd2e124b90540cb67b0
Quang Nguyen <quangnguyen.4298@gmail.com>
2023-08-30 02:35:10 +0700

    Update workflow

---
+++ lib/product-stack.ts
@@ -0,0 +1,73 @@
+import { Duration, Fn, Stack, StackProps } from 'aws-cdk-lib'
+import { Construct } from 'constructs'
+import { AuthorizationType, LambdaIntegration, RestApi, MethodLoggingLevel, ApiKeySourceType } from 'aws-cdk-lib/aws-apigateway'
+import { NodejsFunction, NodejsFunctionProps } from 'aws-cdk-lib/aws-lambda-nodejs'
+import { Runtime } from 'aws-cdk-lib/aws-lambda'
+import { join } from 'path'
+import corsOptions from './cors'
+import {
+    gatewayResponseOptions4XX,
+    gatewayResponseOptions5XX,
+    gatewayResponseOptionsAccessDenied,
+    gatewayResponseOptionsThrottled,
+    gatewayResponseOptionsUnauthorized,
+} from './gateway_response'
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class ProductStack extends Stack {
+    constructor(scope: Construct, id: string, props?: any) {
+        super(scope, id, props)
+
+        const defaultLambdaFnProps: NodejsFunctionProps = {
+            bundling: {
+                externalModules: ['aws-sdk'],
+                minify: true,
+            },
+            entry: join('src/index.ts'),
+            environment: {
+                ENVIRONMENT: props?.buildConfig?.Environment || 'Dev',
+                //     PARTITION_KEY: 'PK',
+                //     SORT_KEY: 'SK',
+                //     INDEX: 'GSI',
+                //     INDEX_PARTITION_KEY: 'data',
+                //     CLOVER_SERVER: getCloverBaseDomain(`${props?.buildConfig?.Environment || 'Dev'}`.toLowerCase()),
+                //     REGION: props?.buildConfig?.AWSProfileRegion || 'ap-southeast-1',
+                //     POS_OPEN_SEARCH_DOMAIN_ENDPOINT: `https://${importedPosDomainEndpoint}`,
+            },
+            runtime: Runtime.NODEJS_16_X,
+        }
+
+        const getProductFn = new NodejsFunction(this, 'GetProductFn', {
+            ...defaultLambdaFnProps,
+            functionName: `${id}-GetProductFn`,
+            handler: 'main',
+            timeout: Duration.seconds(30),
+            memorySize: 256,
+        })
+
+        const getProductIntegration: any = new LambdaIntegration(getProductFn)
+        // const methodOptions: any = { authorizationType: AuthorizationType.CUSTOM }
+
+        const restApi = new RestApi(this, id + '-Api', {
+            deployOptions: {
+                stageName: props.buildConfig?.Environment?.toLowerCase() || 'dev',
+                metricsEnabled: true,
+                loggingLevel: MethodLoggingLevel.INFO,
+                dataTraceEnabled: true,
+                tracingEnabled: true,
+            },
+            defaultCorsPreflightOptions: corsOptions,
+            cloudWatchRole: true,
+            // apiKeySourceType: ApiKeySourceType.AUTHORIZER,
+        })
+
+        restApi.addGatewayResponse(`${id}-denied`, gatewayResponseOptionsAccessDenied)
+        restApi.addGatewayResponse(`${id}-unauthorized`, gatewayResponseOptionsUnauthorized)
+        restApi.addGatewayResponse(`${id}-throttled`, gatewayResponseOptionsThrottled)
+        restApi.addGatewayResponse(`${id}-default4xx`, gatewayResponseOptions4XX)
+        restApi.addGatewayResponse(`${id}-default5xx`, gatewayResponseOptions5XX)
+
+        const clover = restApi.root.addResource('product')
+        clover.addResource('list').addMethod('GET', getProductIntegration)
+    }
+}
---
+++ src/v1/service/clover_service.ts
@@ -0,0 +1,37 @@
+// const ProductFns = {
+//     getProduct: async (event: any, context: any, callback: any) => {
+//         console.log('event', event)
+//         console.log('context', context)
+//         callback(null, {
+//             statusCode: 200,
+//             body: JSON.stringify({
+//                 success: true,
+//                 data: {
+//                     name: 'quang',
+//                     age: 12,
+//                     env: process.env.ENVIRONMENT,
+//                 },
+//             }),
+//         })
+//     },
+// }
+
+// export const ProductService = {
+//     getProduct: ProductFns.getProduct,
+// }
+
+export async function main(event: any) {
+    console.log('event 👉111', event)
+
+    return {
+        body: JSON.stringify({
+            success: true,
+            data: {
+                name: 'quang 123',
+                age: 12,
+                env: process.env.ENVIRONMENT,
+            },
+        }),
+        statusCode: 200,
+    }
+}

55cffe7c6d4ad503e27c81e9cb29201d6ac2ddf4
Quang Nguyen <quangnguyen.4298@gmail.com>
2023-08-29 14:55:12 +0700

    Initial commit

---
+++ test/quangnguyen-98-cloud-api.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Quangnguyen98CloudApi from '../lib/quangnguyen-98-cloud-api-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/quangnguyen-98-cloud-api-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Quangnguyen98CloudApi.Quangnguyen98CloudApiStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
